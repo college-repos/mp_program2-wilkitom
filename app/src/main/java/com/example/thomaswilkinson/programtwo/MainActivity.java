14ea099bb5fc88653f4a114ddee49d340d3a3cb7
wilkitom <31521090+wilkitom@users.noreply.github.com>
2018-09-12 10:44:29 -0600

    Wrote Java code to handle math

--- app/src/main/java/com/example/thomaswilkinson/programtwo/MainActivity.java
+++ app/src/main/java/com/example/thomaswilkinson/programtwo/MainActivity.java
@@ -2,12 +2,13 @@ package com.example.thomaswilkinson.programtwo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    String TAG = "WTF";
     EditText bill, tip, splits;
     Button noRound, roundTotal, roundTip;
 
@@ -28,7 +29,116 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view){
                 Double billAmt = Double.parseDouble(bill.getText().toString());
                 Double tipAmt = Double.parseDouble(tip.getText().toString());
-                Integer splitAmt = Integer.parseInt(tip.getText().toString());
+                Double splitAmt = Double.parseDouble(splits.getText().toString());
+                if(splitAmt == 1)
+                {
+                    Double tipDollarAmt, totalAmt;
+                    Log.v(TAG, billAmt.toString());
+                    tipAmt = tipAmt/100;
+                    tipDollarAmt = billAmt * tipAmt;
+                    totalAmt = billAmt + tipDollarAmt;
+                    Log.v("bill", billAmt.toString());
+                    Log.v("total", totalAmt.toString());
+                    Log.v("tip", tipDollarAmt.toString());
+                }
+                else
+                {
+                    Double tipDollarAmt, totalAmt, splitTotalAmt, splitTipAmt, splitBillAmt;
+                    tipAmt = tipAmt/100;
+                    tipDollarAmt = billAmt * tipAmt;
+                    totalAmt = billAmt + tipDollarAmt;
+                    Log.v("bill", billAmt.toString());
+                    Log.v("total", totalAmt.toString());
+                    Log.v("tip", tipDollarAmt.toString());
+                    splitTotalAmt = totalAmt/splitAmt;
+                    splitTipAmt = tipDollarAmt/splitAmt;
+                    splitBillAmt = billAmt/splitAmt;
+                    Log.v("SplitBill",splitBillAmt.toString());
+                    Log.v("SplitTotal", splitTotalAmt.toString());
+                    Log.v("SplitTip", splitTipAmt.toString());
+                }
+            }
+        });
+
+
+        roundTotal.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Double billAmt = Double.parseDouble(bill.getText().toString());
+                Double tipAmt = Double.parseDouble(tip.getText().toString());
+                Double splitAmt = Double.parseDouble(splits.getText().toString());
+                if(splitAmt == 1)
+                {
+                    Double tipDollarAmt, totalAmt;
+                    Log.v(TAG, billAmt.toString());
+                    tipAmt = tipAmt/100;
+                    tipDollarAmt = billAmt * tipAmt;
+                    totalAmt = billAmt + tipDollarAmt;
+                    long roundedTotalAmt;
+                    roundedTotalAmt = Math.round(totalAmt);
+                    Log.v("bill", billAmt.toString());
+                    Log.v("roundedTotal", String.valueOf(roundedTotalAmt));
+                    Log.v("tip", tipDollarAmt.toString());
+                }
+                else
+                {
+                    Double tipDollarAmt, totalAmt, splitTotalAmt, splitTipAmt, splitBillAmt;
+                    tipAmt = tipAmt/100;
+                    tipDollarAmt = billAmt * tipAmt;
+                    totalAmt = billAmt + tipDollarAmt;
+                    Log.v("bill", billAmt.toString());
+                    Log.v("total", totalAmt.toString());
+                    Log.v("tip", tipDollarAmt.toString());
+                    splitTotalAmt = totalAmt/splitAmt;
+                    splitTipAmt = tipDollarAmt/splitAmt;
+                    splitBillAmt = billAmt/splitAmt;
+                    long roundedSplitTotal;
+                    roundedSplitTotal = Math.round(splitTotalAmt);
+                    Log.v("SplitBill",splitBillAmt.toString());
+                    Log.v("RoundedSplitTotal", String.valueOf(roundedSplitTotal));
+                    Log.v("SplitTip", splitTipAmt.toString());
+                }
+            }
+        });
+
+
+        roundTip.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Double billAmt = Double.parseDouble(bill.getText().toString());
+                Double tipAmt = Double.parseDouble(tip.getText().toString());
+                Double splitAmt = Double.parseDouble(splits.getText().toString());
+                if(splitAmt == 1)
+                {
+                    Double tipDollarAmt, totalAmt;
+                    Log.v(TAG, billAmt.toString());
+                    tipAmt = tipAmt/100;
+                    tipDollarAmt = billAmt * tipAmt;
+                    totalAmt = billAmt + tipDollarAmt;
+                    long roundedTipAmt;
+                    roundedTipAmt = Math.round(tipDollarAmt);
+                    Log.v("bill", billAmt.toString());
+                    Log.v("Total", totalAmt.toString());
+                    Log.v("RoundedTip", roundedTipAmt.toString());
+                }
+                else
+                {
+                    Double tipDollarAmt, totalAmt, splitTotalAmt, splitTipAmt, splitBillAmt;
+                    tipAmt = tipAmt/100;
+                    tipDollarAmt = billAmt * tipAmt;
+                    totalAmt = billAmt + tipDollarAmt;
+                    Log.v("bill", billAmt.toString());
+                    Log.v("total", totalAmt.toString());
+                    Log.v("tip", tipDollarAmt.toString());
+                    splitTotalAmt = totalAmt/splitAmt;
+                    splitTipAmt = tipDollarAmt/splitAmt;
+                    splitBillAmt = billAmt/splitAmt;
+                    long roundedSplitTip;
+                    roundedSplitTip = Math.round(splitTipAmt);
+                    Log.v("SplitBill",splitBillAmt.toString());
+                    Log.v("SplitTotal", splitTotalAmt.toString());
+                    Log.v("RoundedSplitTip", String.valueOf(roundedSplitTip));
+                }
             }
         });
     }
